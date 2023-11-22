a68e038b80ab7deb0eec14fa0f5191b3bb82e19d
Yadong Zhang <zhyd007@gmail.com>
2023-11-22 14:12:35 +0000

    build canvas

--- pages/canvas.js
+++ pages/canvas.js
@@ -1,4 +1,4 @@
-const { createCanvas, loadImage } = require('@napi-rs/canvas')
+const { createCanvas, loadImage } = require('canvas')
 
 export default function Home() {
     const canvas = createCanvas(200, 200)
