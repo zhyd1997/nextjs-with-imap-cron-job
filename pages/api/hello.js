8fc63d43ba7bdfd136bd57e6f6475ea53ffa6088
Yadong Zhang <zhyd007@gmail.com>
2023-11-24 06:40:21 +0000

    fix: updated pdf url.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,7 +3,7 @@ import { extractPdfTextInNodejsEnv } from "../../test"
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default async function handler(req, res) {
   try {
-    const text = await extractPdfTextInNodejsEnv('/workspaces/nextjs-with-imap-cron-job/Sinwa PO (TP23162914) (2023-11-06 16.50.05).pdf')
+    const text = await extractPdfTextInNodejsEnv('https://www-net.cs.umass.edu/wireshark-labs/Wireshark_Intro_v8.0.pdf')
 
     res.status(200).json({ text })
   } catch (error) {
